c7d591f1132d2e63e2fac2710d365a80a4aabad7
Ruben Thys <ruben.thys@student.odisee.be>
2018-04-20 13:29:12 +0100

    feat(Home): basic design + links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faRss } from '@fortawesome/fontawesome-free-solid';
+import { faTwitter, faGithub } from '@fortawesome/fontawesome-free-brands'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="title">
+          <h1>Ruben Thys</h1>
+        </div>
+        <div className="links">
+          <a href="https://twitter.com/rubenthys22">
+            <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon> Twitter
+          </a>
+          <a href="https://github.com/thysruben">
+            <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> GitHub
+          </a>
+          <a href="https://blog.rubenthys.me">
+            <FontAwesomeIcon icon={faRss}></FontAwesomeIcon> Blog
+          </a>
+        </div>
       </div>
     );
   }
