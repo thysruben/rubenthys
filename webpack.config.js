2ac372a7697eff754d4e21f2f01f9c3f9d274ab0
Ruben Thys <ruben.thys@student.odisee.be>
2020-06-29 13:02:12 +0200

    Initialise project

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        'path': path.resolve(__dirname, 'dist'),
+        'publicPath': '/dist/',
+        'filename': 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                use: 'html-loader'
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.wasm', '.ts', '.tsx', '.mjs', '.cjs', '.js', '.json']
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public/'),
+        port: 3000,
+        publicPath: 'http://localhost:3000/dist/',
+        hotOnly: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
