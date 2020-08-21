95c2b7a95140c84bf510bf2f303917af07edd170
Ruben Thys <ruben.thys@student.odisee.be>
2020-08-21 13:17:03 +0200

    Fix netlify build errors

--- src/views/Skills/index.js
+++ src/views/Skills/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import SkillsCard from '../../components/SkillsCard'
-import Icons from '../../utils/Icons'
-import Icon from '../../components/Icon'
-import Background1 from '../../assets/images/skills-card-background-1.svg'
+// import Icons from '../../utils/Icons'
+// import Icon from '../../components/Icon'
+// import Background1 from '../../assets/images/skills-card-background-1.svg'
 
 import './skills.scss'
 
@@ -15,13 +15,13 @@ class Skills extends Component {
                     <SkillsCard
                         title="Design"
                     >
-                        <Icon src={Icons.GitHub} alt="GitHub"></Icon>
+                        {/* <Icon src={Icons.GitHub} alt="GitHub"></Icon>
                         <Icon src={Icons.VSCode} alt="Visual Studio Code"></Icon>
                         <Icon src={Icons.Photoshop} alt="Adobe Photoshop"></Icon>
                         <Icon src={Icons.Sketch} alt="Sketch App"></Icon>
                         <Icon src={Icons.Trello} alt="Trello"></Icon>
                         <Icon src={Icons.Apple} alt="Apple"></Icon>
-                        <Icon src={Icons.Illustrator} alt="Adobe Illustrator"></Icon>
+                        <Icon src={Icons.Illustrator} alt="Adobe Illustrator"></Icon> */}
                     </SkillsCard>
                     <SkillsCard
                         title="Develop"
