97b7d05bfa1a3437e5a369f1e50b5ac029d19a2a
Ruben Thys <ruben.thys@student.odisee.be>
2020-08-28 15:18:05 +0200

    Revert "Clean up views and remove unused files"
    This reverts commit 59f7d22c9e0286a61d9809393e0ec1381f9f402f.

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
+import Headroom from 'react-headroom'
 
 import Content from '../components/Content'
 import Header from '../components/Header'
 import Home from '../views/Home'
+import About from '../views/About'
 import Skills from '../views/Skills'
 import Work from '../views/Work'
 import Contact from '../views/Contact'
@@ -14,9 +16,12 @@ class App extends Component {
     render() {
         return (
             <div className="app">
-                <Header />
+                {/* <Headroom> */}
+                    <Header />
+                {/* </Headroom> */}
                 <Content>
                     <Home />
+                    <About />
                     <Skills />
                     <Work />
                     <Contact />
