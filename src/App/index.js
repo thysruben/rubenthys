59f7d22c9e0286a61d9809393e0ec1381f9f402f
Ruben Thys <ruben.thys@student.odisee.be>
2020-08-28 11:29:49 +0200

    Clean up views and remove unused files

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
-import Headroom from 'react-headroom'
 
 import Content from '../components/Content'
 import Header from '../components/Header'
 import Home from '../views/Home'
-import About from '../views/About'
 import Skills from '../views/Skills'
 import Work from '../views/Work'
 import Contact from '../views/Contact'
@@ -16,12 +14,9 @@ class App extends Component {
     render() {
         return (
             <div className="app">
-                {/* <Headroom> */}
-                    <Header />
-                {/* </Headroom> */}
+                <Header />
                 <Content>
                     <Home />
-                    <About />
                     <Skills />
                     <Work />
                     <Contact />
