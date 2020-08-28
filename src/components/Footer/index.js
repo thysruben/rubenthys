fed14570693d300945f59f269f4c45aad9e00a73
Ruben Thys <ruben.thys@student.odisee.be>
2020-08-28 23:08:51 +0200

    Add Contact view and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 
 import './footer.scss'
+import githubIcon from '../../assets/icons/github-icon.svg'
 
 class Footer extends Component {
     render() {
         return (
-            <div className="footer"></div>
+            <div className="footer">
+                <div className="footer--container">
+                    <div className="footer--socials">
+                        <a href="https://www.linkedin.com/in/ruben-thys-895545120/" target="_blank">
+                            <img src={githubIcon} alt="Github"/>
+                        </a>
+                    </div>
+                    <div className="footer--text">
+                        <p>Design and coding done by me! 😎</p>
+                        <a href="mailto:hello@rubenthys.dev" target="-blank">hello@rubenthys.dev</a>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
--- src/views/Contact/index.js
+++ src/views/Contact/index.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 
 import './contact.scss'
+import twitterIcon from '../../assets/icons/twitter-white.svg'
+import linkedInIcon from '../../assets/icons/linkedin-white.svg'
+import mailIcon from '../../assets/icons/mail.svg'
 
 class Contact extends Component {
     render() {
         return (
-            <div className="contact"></div>
+            <div className="contact">
+                <div className="contact--container">
+                    <div className="contact--card">
+                        <h3>Get in touch with me!</h3>
+                        <div className="contact--socials">
+                            <a href="https://www.linkedin.com/in/ruben-thys-895545120/" target="_blank">
+                                <img src={linkedInIcon} alt="LinkedIn"/>
+                            </a>
+                            <a href="mailto:hello@rubenthys.dev">
+                                <img src={mailIcon} alt="Mail" target="_blank"/>
+                            </a>
+                            <a href="https://twitter.com/rubenthys22">
+                                <img src={twitterIcon} alt="Twitter" target="_blank"/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
