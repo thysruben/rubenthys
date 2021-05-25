19719fff5790c1e4bef01a1081bb8a4c9fe24a91
Ruben Thys <rubenthys22@gmail.com>
2021-05-25 11:32:57 +0200

    Change skills content

--- src/views/Skills/index.js
+++ src/views/Skills/index.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
-import SkillCards from '../../components/SkillCards'
+import React, { Component } from 'react';
+import SkillCards from '../../components/SkillCards';
 
-import './skills.scss'
-import skills from '../../utils/Skills'
+import './skills.scss';
+import skills from '../../utils/Skills';
 
 class Skills extends Component {
-    render() {
-        return (
-            <div className="skills">
-                <div className="skills--container">
-                    <h3>Skills</h3>
-                    <p>My skills and tools I have experience with</p>
-                    <SkillCards
-                        skills={skills}
-                    ></SkillCards>
-                </div>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className='skills'>
+				<div className='skills--container'>
+					<h3>Skills</h3>
+					<p>Skills and tools I have experience with.</p>
+					<SkillCards skills={skills}></SkillCards>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default Skills
+export default Skills;
